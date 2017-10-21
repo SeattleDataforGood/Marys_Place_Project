3e21bd6000a7807ec758cccc9057624700532231
Alrun Burgmeier <allmensch@gmx.net>
2017-10-21 12:12:54 -0700

    second edits

--- Apps/Marys_Place_Uploader/MainWindow.xaml.cs
+++ Apps/Marys_Place_Uploader/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,46 @@ namespace Marys_Place_Uploader
 
         private void Start_Upload_Click(object sender, RoutedEventArgs e)
         {
-            
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = $"Server=tcp:test-server-1-mml.database.windows.net,1433;Initial Catalog=test-db-1;Persist Security Info=False;User ID={AzureDBUserName.Text};Password={AzureDBPassword.Password}";
+            cnn = new SqlConnection(connetionString);
+            try {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                string[] lines = System.IO.File.ReadAllLines((string) FileName.Content);
+
+                foreach (string line in lines)
+                {
+                    // Use a tab to indent each line of the file.
+                    Console.WriteLine("\t" + line);
+
+                    char[] delimiterChars = { ',',';', '\t' };
+
+                 
+
+                    string[] fields = line.Split(delimiterChars);
+
+                    SqlCommand cmd = new SqlCommand("insert into mp_data(shelter, move_in, move_out, moved_from, homeless_status, moved_to, departure_type) value(@0, @1, @2, @3, @4, @5, @6)");
+
+                    int i = 0;
+                    foreach (string f in fields)
+                    {
+                        cmd.Parameters.AddWithValue("@"+i, f);
+                        
+
+                        System.Console.WriteLine(f);
+                        i++;
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                cnn.Close();
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Can not open connection ! ");
+            }
         }
 
         private void Window_Activated(object sender, EventArgs e)
